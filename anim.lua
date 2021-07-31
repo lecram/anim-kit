61e794f03071feb247a25bb89e0f5f82666332a0
Marcel Rodrigues <marcelgmr@gmail.com>
2021-07-31 12:48:58 -0300

    encapsulate modules in package anim

---
+++ anim.lua
@@ -0,0 +1,8 @@
+local anim = {}
+anim.surf = require "surf"
+anim.poly = require "poly"
+anim.aff  = require "aff"
+anim.aa   = require "aa"
+anim.ttf  = require "ttf"
+anim.gif  = require "gif"
+return anim
