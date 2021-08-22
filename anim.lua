5da7c6d2d94dec79267c170d280bf7a1af38a02d
Marcel Rodrigues <marcelgmr@gmail.com>
2021-08-22 17:12:21 -0300

    include map and shp in package

--- anim.lua
+++ anim.lua
@@ -2,7 +2,9 @@ local anim = {}
 anim.surf = require "surf"
 anim.poly = require "poly"
 anim.aff  = require "aff"
+anim.map  = require "map"
 anim.aa   = require "aa"
 anim.ttf  = require "ttf"
+anim.shp  = require "shp"
 anim.gif  = require "gif"
 return anim
